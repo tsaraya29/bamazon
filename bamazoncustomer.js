e5d21549322433f638947b79c8fbabcbbbd97130
T Saraya <saratgrffn@gmail.com>
2019-02-28 15:09:37 -0600

    working

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,7 +11,7 @@ var con = mysql.createConnection({
   con.connect(function(err){
       if (err) throw err;
       console.log ('Connection Successful');
-      var sql = "SELECT product_name, price FROM products;"
+      var sql = "SELECT item_id, product_name, price FROM products;"      
      
   con.query(sql, function(err, result){
       if (err) throw err;
@@ -19,10 +19,7 @@ var con = mysql.createConnection({
     });
     
   });
-
-  //Begin asking question:
-  inquirer
-  .prompt([
+  const ask_Question = inquirer.prompt([
     {
       type: "list",
       message: "What would you like to purchase?",
@@ -33,5 +30,9 @@ var con = mysql.createConnection({
   .then(function(inquirerResponse){
     console.log("You have selected ", inquirerResponse.name);
     });
+
+  //Begin asking question:
+  ask_Question();
+  
 
 
