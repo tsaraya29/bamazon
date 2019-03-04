796803870154fe1154c4676fc00013cd3e6110ae
T Saraya <saratgrffn@gmail.com>
2019-03-04 14:05:40 -0600

    working file

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,28 +11,47 @@ var con = mysql.createConnection({
   con.connect(function(err){
       if (err) throw err;
       console.log ('Connection Successful');
-      var sql = "SELECT item_id, product_name, price FROM products;"      
+      var sql = "SELECT item_id, product_name, price FROM products;"
+      // var item_list = "SELECT product_name FROM products;"
+      console.log("Bamazon Product Listing:");
      
   con.query(sql, function(err, result){
       if (err) throw err;
-      console.log ('Bamazon product listing: /n' , result);
+      console.log(result);
+      ask_Customer();
     });
     
   });
-  const ask_Question = inquirer.prompt([
-    {
-      type: "list",
-      message: "What would you like to purchase?",
-      choices: ["choice1", "choice2","choice3"],
-      name: "item"
-    }
-  ])
-  .then(function(inquirerResponse){
-    console.log("You have selected ", inquirerResponse.name);
-    });
-
   //Begin asking question:
-  ask_Question();
+  function ask_Customer (){
+    inquirer.prompt([
+    {
+      type: "input",
+      name: "choice",
+      message: "What would you like to purchase? (enter item ID)",       
+    },
+    { 
+      type: "input",
+      name: "quantity",
+      message: "How many would you like to purchase?"
+    }   
+  ])
+  .then(function(response){
+    con.query("SELECT stock_quantity FROM products WHERE item_id=?", [response.choice],function(err, result){
+      if (err) throw err;
+      if (result[0].stock_quantity < response.quantity)
+      console.log("Sorry, we don't have enough");
+      });
+    // var stuff= con.query ("SELECT * FROM products WHERE item_id=?",
+    //   [response.choice]);
+    //    console.log (stuff);
+    // if (stock_quanity < response.quantity) {
+    //   console.log ("Sorry, we only have " + products.stock_quanity + "left");
+    // }
+    // console.log("You have selected ", response.product_name);
   
+    // console.log("You have selected ", Response.choice);
+    });  
+  };
 
-
+ 
